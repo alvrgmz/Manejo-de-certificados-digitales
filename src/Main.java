7c41c80a93008e4f40515f8204e74ac70a947976
Álvaro G <gmzprzalvaro@gmail.com>
2024-02-25 19:25:25 +0100

    Archivo jar creado

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.security.cert.X509Certificate;
 public class Main {
 
     public static void main(String[] args) {
-        String filePath = "certificado.crt"; // Ruta del archivo de certificado
-        X509Certificate certificate = cargarCertificado(filePath);
-        if (certificate != null) {
-            mostrarInformacionCertificado(certificate);
-            boolean esValido = verificarAutenticidadCertificado(certificate);
+        String rutaArchivo = "certificado.crt"; // Ruta del archivo de certificado
+        X509Certificate certificado = cargarCertificado(rutaArchivo);
+        if (certificado != null) {
+            mostrarInformacionCertificado(certificado);
+            boolean esValido = verificarAutenticidadCertificado(certificado);
             System.out.println("El certificado es válido: " + esValido);
         } else {
             System.out.println("No se pudo cargar el certificado.");
@@ -17,9 +17,11 @@ public class Main {
     }
 
     // Método para cargar un certificado desde un archivo
-    public static X509Certificate cargarCertificado(String filePath) {
-        try (FileInputStream fis = new FileInputStream(filePath)) {
+    public static X509Certificate cargarCertificado(String rutaArchivo) {
+        try (FileInputStream fis = new FileInputStream(rutaArchivo)) {
+            //Creamos una instancia de la clase CertificateFactory con el tipo de certificado que queremos cargar
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            //Retornamos el certificado cargado mediante el método de la clase CertificateFactory
             return (X509Certificate) cf.generateCertificate(fis);
         } catch (Exception e) {
             e.printStackTrace();
@@ -28,19 +30,19 @@ public class Main {
     }
 
     // Método para mostrar la información del certificado
-    public static void mostrarInformacionCertificado(X509Certificate certificate) {
-        System.out.println("Propietario: " + certificate.getSubjectDN());
-        System.out.println("Emisor: " + certificate.getIssuerDN());
-        System.out.println("Número de serie: " + certificate.getSerialNumber());
-        System.out.println("Fecha de emisión: " + certificate.getNotBefore());
-        System.out.println("Fecha de vencimiento: " + certificate.getNotAfter());
-        System.out.println("Algoritmo de firma: " + certificate.getSigAlgName());
+    public static void mostrarInformacionCertificado(X509Certificate certificado) {
+        System.out.println("Propietario: " + certificado.getSubjectDN());
+        System.out.println("Emisor: " + certificado.getIssuerDN());
+        System.out.println("Número de serie: " + certificado.getSerialNumber());
+        System.out.println("Fecha de emisión: " + certificado.getNotBefore());
+        System.out.println("Fecha de vencimiento: " + certificado.getNotAfter());
+        System.out.println("Algoritmo de firma: " + certificado.getSigAlgName());
     }
 
     // Método para verificar la autenticidad del certificado
-    public static boolean verificarAutenticidadCertificado(X509Certificate certificate) {
+    public static boolean verificarAutenticidadCertificado(X509Certificate certificado) {
         try {
-            certificate.verify(certificate.getPublicKey());
+            certificado.verify(certificado.getPublicKey());
             return true;
         } catch (Exception e) {
             return false;
